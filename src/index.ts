d45b292dc23240b6a494472c02ac43808f0ddf2c
Liul Alemayehu <liulalemx@gmail.com>
2023-01-19 17:42:28 +0300

    Create welcome message and fetch templates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-console.log("Hi there");
+#!/usr/bin/env node
+
+import * as fs from "fs";
+import * as path from "path";
+import chalk from "chalk";
+import inquirer from "inquirer";
+// import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+const __dirname = path.resolve();
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome(callback: any) {
+  figlet(
+    "Create Static Site",
+    { font: "Larry 3D" },
+    async function (err: any, data: any) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+
+      const introTitle = chalkAnimation.rainbow(data);
+      await sleep();
+      introTitle.stop();
+      console.log(
+        chalk.blue.bold("The best way to start a modern static site \n")
+      );
+      callback();
+    }
+  );
+}
+
+function generateTemplate() {
+  const CHOICES = fs.readdirSync(path.join(__dirname, "templates"));
+
+  const QUESTIONS = [
+    {
+      name: "template",
+      type: "list",
+      message: "What project template would you like to generate?",
+      choices: CHOICES,
+    },
+    {
+      name: "name",
+      type: "input",
+      message: "Project name:",
+    },
+  ];
+
+  inquirer.prompt(QUESTIONS).then((answers) => {
+    console.log(answers);
+  });
+}
+
+console.clear();
+await welcome(function () {
+  generateTemplate();
+});
