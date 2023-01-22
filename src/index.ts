79f8fa804962e07bd4d111bf15bba88ef12c093d
Liul Alemayehu <liulalemx@gmail.com>
2023-01-22 15:04:59 +0300

    Generate files from templates

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ interface CliOptions {
 const __dirname = path.resolve();
 const CURR_DIR = process.cwd();
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+const SKIP_FILES = ["node_modules", ".template.json"];
 
 async function welcome(callback: any) {
   figlet(
@@ -75,6 +76,8 @@ function generateTemplate() {
     if (!createProject(tartgetPath)) {
       return;
     }
+
+    createDirectoryContents(templatePath, projectName);
   });
 }
 
@@ -90,6 +93,37 @@ function createProject(projectPath: string) {
   fs.mkdirSync(projectPath);
 
   return true;
+}
+
+function createDirectoryContents(templatePath: string, projectName: string) {
+  // read all files/folders (1 level) from template folder
+  const filesToCreate = fs.readdirSync(templatePath);
+  // loop each file/folder
+  filesToCreate.forEach((file) => {
+    const origFilePath = path.join(templatePath, file);
+
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
+
+    // skip files that should not be copied
+    if (SKIP_FILES.indexOf(file) > -1) return;
+
+    if (stats.isFile()) {
+      // read file content and transform it using template engine
+      let contents = fs.readFileSync(origFilePath, "utf8");
+      // write file to destination folder
+      const writePath = path.join(CURR_DIR, projectName, file);
+      fs.writeFileSync(writePath, contents, "utf8");
+    } else if (stats.isDirectory()) {
+      // create folder in destination folder
+      fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+      // copy files/folder inside current folder recursively
+      createDirectoryContents(
+        path.join(templatePath, file),
+        path.join(projectName, file)
+      );
+    }
+  });
 }
 
 console.clear();
